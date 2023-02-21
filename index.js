75824556c07fbffc82dba4dcf7bd7bce6735831a
pharmer101 <117358586+pharmer101@users.noreply.github.com>
2023-02-21 22:40:03 +0000

    Added updated Javascript files

--- index.js
+++ index.js
@@ -8,68 +8,128 @@ const inquirer = require('inquirer');
 //testing fs append working 
 console.log("Node Running")
 const fs = require("fs");
-// console.log("logfile being created")
-// fs.appendFile("example_file.txt", "World", (err) => {
-//     if (err) {
-//       console.log(err);
-//     }
-//     else {
-//       // Get the file contents after the append operation
-//       console.log("\nFile Contents of file after append:",
-//         fs.readFileSync("example_file.txt", "utf8"));
-//     }
-//   });
-//--------------------------------------------- set up inputs questions from inquery---------------------------------
+var badge
 
-//*** TO BE ADDED TO SECTIONS IN DOC */
+//--------------------------------------------- Get input from user using inquirer package--------------------------------
 
-// Title of project
-// Description of project
-//installation instructions
-//usage instructions
-//contribution guidelines
-//test instructions
-//Licence type (turns into a badge)
-//github username
-//email address
-
-// const title = process.argv[2];
-// const description = process.argv[3];
-// const instalInstruct = process.argv[4];
-// const contribute = process.argv[5];
-// const testInstruct = process.argv[6];
-// const licenceType = process.argv[7];
-// const gitUser = process.argv[8];
-// const email = process.argv[9];
-
-
-
-//--------------------------------------------------print out answers to REAMDME-----------------------------------------
 
 inquirer
-inquirer
-//   .prompt([
-//     {
-//       name: 'faveReptile',
-//       message: 'What is your favorite reptile?'
-//     },
-//   ])
-//   .then(answers => {
-//     console.info('Answer:', answers.faveReptile);
-//   });
-
   .prompt([
     {
-      name: 'faveReptile',
-      message: 'What is your favorite reptile?'
+      name: 'title',
+      message: 'What is the title of your project?'
     },
+
+    {
+        name: 'description',
+        message: 'Please provide a description of your project '
+    },
+    {
+        name: 'installation',
+        message: 'Please provide a installation instructions for your project '
+    },
+    {
+        name: 'usage',
+        message: 'Please explain how to use your project '
+    },
+    {
+        name: 'contribute',
+        message: 'Please explain how people can contribute to your project '
+    },
+    {
+        name: 'test',
+        message: 'Please explain how the projected is tested '
+    },
+    // {
+    //     name: 'licence',
+    //     message: 'what kind of licence does the project have? '
+    // },
+    {
+        name: 'licence',
+        type: "list",
+        choices:["MIT","GPL","IPL"],
+        message: 'what kind of licence does the project have? '
+    },
+    {
+        name: 'gitName',
+        message: 'what is your github username? '
+    },
+    {
+        name: 'email',
+        message: 'what is your email address? '
+    },
+
+// -------------------------------add answers to variables --------------------
   ])
   .then(answers => {
     //console.log('Your fav reptile is ', answers.faveReptile);
-    const favReptile = answers.faveReptile;
-    console.log(favReptile);
+    const title = answers.title;
+    const description = answers.description;
+    const installation = answers.installation;
+    const usage = answers.usage;
+    const contribute = answers.contribute;
+    const test = answers.test;
+    const licence = answers.licence;
+    const gitName = answers.gitName;
+    const email = answers.email;
+   
+    //----------------------------Create markdown text with answers from user---------------------------------------
+    
+    
+    if (licence ==="MIT") {
+        badge = "[License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+      } else if (licence === "GPL") {
+        badge = "[License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+      } else {
+        badge = "[License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)"
+      }
 
-        fs.appendFile("example_file.txt", favReptile, (err) => {
+
+
+   
+// const badge = "[License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+
+  
+
+
+    const readmeText = 
+    `
+
+# [!${badge}
+
+# ${title}
+## CONTENTS
+
+- [Description](#description)
+- [Installation](#installation)
+- [How to Use Use](#how-to-use-use)
+- [How to contribute](#how-to-contribute)
+- [Testing](#testing)
+- [Licence](#licence)
+- [Github Name](#github-name)
+- [Email](#email)
+
+
+## Description
+${description}
+## Installation
+${installation}
+## How to Use Use
+${usage}
+## How to contribute
+${contribute}
+## Testing
+${test}
+## Licence
+${licence}
+## Questions
+## Github Name
+${gitName}
+## Email
+${email} `
+ //--------------------- Create readme markdown file using the fs.writeFile method-----------------------
+
+    fs.writeFile("readMe.md", SampleReadmeText, (err) => {
         if (err) {
         console.log(err);
         }
@@ -79,5 +139,6 @@ inquirer
         //     fs.readFileSync("example_file.txt", "utf8"));
         // }
     });
+
 
   });
